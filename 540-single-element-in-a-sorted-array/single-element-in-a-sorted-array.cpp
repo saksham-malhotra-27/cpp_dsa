46185a581e92cd43635fc5911d483357455bb266
Saksham Malhotra <147790402+saksham-malhotra-27@users.noreply.github.com>
2024-07-10 00:37:27 +0530

    Time: 21 ms (41.02%) | Memory: 24.5 MB (85.82%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-    int n = nums.size();
-    if(n==1) return nums[0];
-    if(nums[0]!=nums[1]) return nums[0];
-    if(nums[n-1]!=nums[n-2]) return nums[n-1];
-    int start = 1 , end = n-2; int  mid=0;
-    //1,1,2,2,3,4,4,8,8]
-    //0 1 2 3 4 5 6 7 8 
-    while( start <= end){
-        int mid =  start + (end-start)/2;
-        if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-            return nums[mid];
+    int singleNonDuplicate(vector<int>& arr) {
+        int low = 0, high = arr.size()-1;
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if( (mid == 0 && arr[mid] != arr[mid+1]) || (mid == arr.size()-1 && arr[mid]!= arr[mid+1]) || (arr[mid] != arr[mid-1] && arr[mid+1] != arr[mid]) ){
+                return arr[mid];
+            }
+            if( mid % 2 != 0){
+                if(arr[mid] == arr[mid-1]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            } else {
+                if(arr[mid]==arr[mid+1]){
+                    low = mid+ 1;
+                } else {
+                    high = mid -1 ; 
+                }
+            }
         }
-        else if((mid % 2 == 0 && nums[mid+1]==nums[mid] )|| (mid%2!=0 && nums[mid]==nums[mid-1])){
-            start = mid+1;
-        }
-        else if ((mid %2 == 0 && nums[mid+1]!= nums[mid]) || (mid%2!=0 && nums[mid]!=nums[mid-1])){
-            end = mid-1;
-        }
-    
+        return arr[low];
     } 
-    return -1;
-    }
 };
 
 /*
