d6366c8974f617832b070e0b46d3b9e55eaf7018
Saksham Malhotra <147790402+saksham-malhotra-27@users.noreply.github.com>
2024-07-23 23:07:30 +0530

    Time: 9 ms (27.07%) | Memory: 8.7 MB (67.17%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,19 +4,26 @@ public:
         if(s.length() != t.length()){
             return false;
         }
-        map <char,char> mp;
-        map <char,char> mpp;
+        map <char,char> mp; // s to t 
+        map <char,char> mpp; // t to s 
+
         for(int i=0; i<s.length(); i++){
-            if((mp.find(s[i])==mp.end())){
-                if(mpp.find(t[i])==mpp.end()){
-                mp[s[i]] = t[i]; mpp[t[i]] = s[i];
+            if((mp.find(s[i])==mp.end())){ // b is not present  
+               // fab   ghg
+                if(mpp.find(t[i])==mpp.end()){ // g is not present 
+                   mp[s[i]] = t[i]; mpp[t[i]] = s[i];
                 }
-                else {
+                else { // g is present 
                     return false;
                 }
-            } else {
+
+            } 
+            //  it -> ['o','a'], iit-> ['r', 'h']
+            else { // second o is present 
                 auto it  = mp.find(s[i]); auto iit = mpp.find(t[i]);
                 if(it->second!=iit->first && it->first!=iit->second){
+                    // 'a'  != 
+                    // map ['a'] = 'b', it.first 'a' it.second 'b'
                     return false;
                 }
             }
