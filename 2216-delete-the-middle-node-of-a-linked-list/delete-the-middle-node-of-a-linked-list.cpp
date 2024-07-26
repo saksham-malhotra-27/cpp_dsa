279743663574183e5955fcde6087f2ed9d9295d7
Saksham Malhotra <147790402+saksham-malhotra-27@users.noreply.github.com>
2024-07-26 21:48:09 +0530

    Time: 598 ms (19.19%) | Memory: 299.8 MB (15.51%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -16,14 +16,17 @@ public:
         }
 
         ListNode *slow=head;
-        ListNode *fast=head;
         ListNode * prev=nullptr;
+        // if we do this that fast ko humne head start diya, then the prev in one case would be nullptr when we are accessing it:
+        // like [1,2] here slow = 1, fast is 2 and prev is null, now in this case , the loop will not work, therefore, the value of prev will still remain null and then we are accessing it, so to remove this, what we can simply do is not give fast a head start, so that slow +1 and therefore prev will be initialized 
+        ListNode *fast=head;
         while(fast && fast->next){
             prev=slow;
             slow=slow->next;
             fast=fast->next->next;
         }
-        prev->next = prev->next->next;
+       prev->next = prev->next->next;
+       //slow->next = slow->next->next;
         return head;
     }
 };
