6a0bb1588c69756192aac5d987a4f2a40888ccf6
Saksham Malhotra <147790402+saksham-malhotra-27@users.noreply.github.com>
2024-08-31 22:09:22 +0530

    Time: 5 ms (62.17%) | Memory: 16.2 MB (67.82%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector <int> v;  int start = 0 , end = nums.size()-1; 
-         int ans1=-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target){
-                ans1 = mid; end = mid-1;
+        int n = nums.size(); //size of the array
+        int low = 0, high = n - 1;
+        // Perform the steps:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target) {
+                if(nums[mid]==target && ((mid!=0 && nums[mid]!=nums[mid-1]) || (mid==0))){
+                    int i= mid;
+                    int j= mid;
+                    while(mid<nums.size() && nums[mid]==target){
+                        j=mid;
+                        mid++;
+                    }
+                    return {i,j};
+                }
+                else {
+                    high = mid - 1;
+                }
             }
-            else if(nums[mid]>target){
-                end = mid -1;
-            }
-            else {
-                start = mid+1 ; 
-            }
+            else low = mid + 1;
         }
-        if(ans1==-1){return {-1,-1};}
-        start = 0 , end = nums.size()-1;  int ans2=0;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target){
-                ans2 = mid; start=mid+1;
-            }
-            else if(nums[mid]>target){
-                end = mid -1;
-            }
-            else {
-                start = mid+1 ; 
-            }
+        return {-1,-1};
         }
-        if(ans1==ans2){ v.push_back(ans1);v.push_back(ans1); return v;}
-        v.push_back(ans1); v.push_back(ans2);
-        return v;
-    }
 };
