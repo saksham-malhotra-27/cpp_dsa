877b2960edf4dfc1f315d9b9d4b21f755de4062c
Saksham Malhotra <147790402+saksham-malhotra-27@users.noreply.github.com>
2024-09-01 23:01:58 +0530

    Time: 3 ms (97.26%) | Memory: 8.8 MB (77.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,22 @@
-using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        if(s.length() != t.length()){
+            return false;
+        }
+        if(t.length()==0) return true;
+        int count[26]; 
+        
+        for(int i=0; i<s.length(); i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+        }
+
+        for(auto i: count){
+            if(i>0){
+                return false;
+            }
+        }
+        return true;
     }
 };
