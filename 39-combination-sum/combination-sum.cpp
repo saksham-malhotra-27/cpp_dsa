1195e230c919c5e276c4c5f285fcbf1146094729
Saksham Malhotra <147790402+saksham-malhotra-27@users.noreply.github.com>
2024-09-28 12:13:13 +0530

    Time: 85 ms (8.16%) | Memory: 73.4 MB (5.14%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
 
     vector<vector<int>> summ (vector<int>& candidates, int i, int target, vector<int> v,  int sum, vector<vector<int>> ans){
-        if(sum>=target || i>= candidates.size()){
-            if(sum==target) ans.push_back(v);
+        if(sum >= target || i>=candidates.size()){
+            if(sum==target) {ans.push_back(v);}
             return ans;
         }
-        auto a = summ(candidates,i+1, target, v, sum, ans);
         v.push_back(candidates[i]);
-        auto b = summ(candidates, i, target, v, sum + candidates[i], ans);
-        //auto c = summ(candidates, i+1, target, v, sum + candidates[i], ans);
-        //a.insert(a.end(), c.begin(), c.end());
-        // didn't consider this because a + b together comprises of c as well 
+        auto a = summ(candidates, i, target, v, sum+candidates[i], ans);
+        v.pop_back();
+        auto b = summ(candidates, i+1, target, v, sum, ans);
         a.insert(a.end(), b.begin(), b.end());
         return a;
     }
