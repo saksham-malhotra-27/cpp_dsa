978f278bd643346a8060d04d9616a78f59934a9f
Saksham Malhotra <147790402+saksham-malhotra-27@users.noreply.github.com>
2024-10-17 17:28:55 +0530

    Time: 9 ms (26.65%) | Memory: 18.7 MB (21.28%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -15,22 +15,24 @@ public:
         if(!root){
             return 0;
         }
-        queue<pair<TreeNode*, unsigned long long>> q;
-        q.push({root, 1});
+        queue<pair<TreeNode*, long>> q;
+        q.push({root, 0});
         int mwidth = 0;
         while(q.size()){
             int size = q.size(); 
-            unsigned long long start = q.front().second; 
-            unsigned long long end = 0; 
+            long start = q.front().second; 
+            long end = 0; 
             for(int i=0; i<size; i++){
                 TreeNode* n = q.front().first; 
-                unsigned long long val = q.front().second;
+                long val = q.front().second - start;
+                // unsigned long long val = q.front().second;
                 q.pop(); 
                 if(n->left)  q.push({n->left,  2*val});
                 if(n->right) q.push({n->right, 2*val + 1 });
                 end = val; 
             }
-            mwidth = max(mwidth, (int)(end-start)+1);
+            //mwidth = max(mwidth, (int)(end-start)+1);
+            mwidth = max(mwidth, (int)end+1);
         }
         return mwidth; 
     }
